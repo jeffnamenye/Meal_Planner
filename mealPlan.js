c2f2def2107bfd3debd1bf4751c7da5a4dac9362
Jeff Namenye <jeffnamenye@hotmail.com>
2017-07-03 20:07:29 -0400

    Set up

---
+++ mealPlan.js
@@ -0,0 +1,32 @@
+const test = require('./test.js');
+
+
+
+//exports my meal
+module.exports.arr = []
+module.getMeal = function('Meal ID', 'Ingrediant', 'Amount', 'Unit') {
+    return('1445asd', 'Chicken', '8', 'oz');
+}
+
+
+
+
+
+//exports my meals
+
+
+
+
+//created meals
+
+
+
+
+
+// updated meal
+
+
+
+
+
+//deletes my meal
---
+++ test.js
@@ -0,0 +1,5 @@
+const mealPlan = require('./mealPlan.js');
+exports.getMeal = function(getMeal) => {
+if(getMeal){
+  console.log(getMeal);
+}
